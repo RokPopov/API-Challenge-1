320f72b5888a26b79b9ca2bcd23f28bf2d6138fe
rokpopov <rokpopov90@gmail.com>
2021-06-13 11:17:52 +0200

    fetch data from API

--- src/script.js
+++ src/script.js
@@ -4,10 +4,20 @@ const API_ENDPOINT = 'https://yesno.wtf/api';
 /**
  * STEPS:
  *
- * 1. Create a fetchAnswer function and call the API
- * 2. Output the API's response
- * 3. Attach fetchAnswer to an event listener
- * 4. Clear output after 3 seconds
- * 5. Optional: add loading/error states
+ * [x] 1. Create a fetching function and call the API
+ * [] 2. Output the API's response
+ * [] 3. Attach fetchData to an event listener
+ * [] 4. Clear output after 3 seconds
+ * [] 5. Optional: add loading/error states
  *
  */
+
+const fetchData = () => {
+  fetch(API_ENDPOINT)
+    .then(res => res.json())
+    .then(res => console.log(res.data))
+    
+
+}
+
+fetchData();
